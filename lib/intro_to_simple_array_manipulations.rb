7a4eaa964ef1ce40f82e6acddec3910ce2e503bd
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-28 21:59:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,6 +29,20 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
+
+def using_unique(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete = string
+end
+
+
   
   
   
